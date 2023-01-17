1885dd7566e2aa155a52c2276b9476c415fe8c4b
David <82851461+DavidLMP1@users.noreply.github.com>
2023-01-17 14:25:54 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const qrcode = require("qrcode");
+
+const url = '1002';
+
+const run = async () => {
+  const QR = await qrcode.toDataURL(url);
+  const htmlContent = `
+  <div style="display: flex; justify-content: center; align-items: center;">
+  <h2>Hola Bebe</h2>
+  <img src="${QR}">
+  </div>
+  `;
+  fs.writeFileSync("./index.html", htmlContent);
+};
+
+run();
